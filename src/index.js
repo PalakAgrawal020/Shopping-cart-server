fe9e38786323ad3cf716fc56e65405cad033d206
PalakAgrawal020 <palakagrawal020@gmail.com>
2024-11-19 13:07:23 +0530

    setup apierror, apiresponse, asynchandler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// require('dotenv').config({path: './env'})
-
-
 import dotenv from "dotenv"
+import { app } from "./app.js"
 import connectDB from './db/index.js'
 
 
@@ -11,7 +9,14 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!", err);
+})
 
 
 
